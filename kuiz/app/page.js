fd13e16206d2ee3a14d38e28a2b1472d218135ba
Naowak <yannis.bendiouis@gmail.com>
2025-02-15 13:32:27 +0100

    menu glissan

--- kuiz/app/page.js
+++ kuiz/app/page.js
@@ -3,6 +3,7 @@
 import questions from './question-reponse.json';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { IoOptionsOutline, IoCloseOutline } from "react-icons/io5";
 
 export default function Home() {
 
@@ -18,6 +19,7 @@ export default function Home() {
     'Art & Culture': true,
     'Société & Economie': true,
   });
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
 
   const buttonColors = {
     'Sciences & Technologie': 'bg-green-800',
@@ -56,7 +58,7 @@ export default function Home() {
       setSelectedAnswer(null);
       setPreviousQuestions(currentQuestion);
       pickRandomQuestion(); // Choisit une nouvelle question
-    }, 1000);
+    }, 1500);
   };
 
   // Toggle category selection
@@ -77,7 +79,7 @@ export default function Home() {
   const pickRandomQuestion = () => {
     const activeCategories = Object.keys(categories).filter((cat) => categories[cat]);
     const filteredQuestions = questions.filter((q) => activeCategories.includes(q.category));
-    
+
     if (filteredQuestions.length === 0) {
       alert("Aucune question disponible pour les catégories sélectionnées.");
       return;
@@ -104,24 +106,46 @@ export default function Home() {
   return (
     <div className={`flex items-start pt-8 justify-center min-h-screen ${currentQuestion?.category ? bgColors[currentQuestion.category] : 'bg-white'}`}>
 
+      {/* Top left corner : floating button to show menu/config icon */}
+      <button
+        className='fixed top-2 right-2 md:top-8 md:right-8 p-3'
+        onClick={() => setIsPanelOpen(!isPanelOpen)}
+      >
+        {isPanelOpen ? null : <IoOptionsOutline color='black' size={30} />}
+      </button>
+
+      {/* Side Panel */}
+      <div className={`fixed top-0 right-0 h-full bg-slate-50 shadow-lg transform transition-transform duration-300 ${isPanelOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className="p-4 mt-2">
+
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-3xl font-bold text-black">Menu</h1>
+            <button onClick={() => setIsPanelOpen(false)} className=''>
+              <IoCloseOutline color='black' size={24} />
+            </button>
+          </div>
+
+          <h2 className="text-xl font-bold text-black mb-2">Sélectionnez les catégories</h2>
+          <div className="flex flex-col space-y-2">
+            {Object.keys(categories).map((category) => (
+              <button
+                key={category}
+                onClick={() => toggleCategory(category)}
+                className={`p-2 rounded-lg text-white text-sm font-medium shadow-md transition-all ${
+                  buttonColors[category]
+                } ${categories[category] ? 'opacity-100' : 'opacity-50'}`}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+          
+        </div>
+      </div>
+
       <div className="w-full max-w-md lg:max-w-lg p-2">
         {/* Titre */}
         <h1 className="text-center text-5xl font-bold text-black mb-8">Le Kuiz</h1>
-
-        {/* Boutons de catégories */}
-        <div className="flex flex-wrap justify-center gap-2 mb-8">
-          {Object.keys(categories).map((category) => (
-            <button
-              key={category}
-              onClick={() => toggleCategory(category)}
-              className={`p-2 rounded-lg text-white text-sm font-medium shadow-md transition-all ${
-                buttonColors[category]
-              } ${categories[category] ? 'opacity-100' : 'opacity-50'}`}
-            >
-              {category}
-            </button>
-          ))}
-        </div>
 
         {/* Question */}
         <p className="text-center font-bold text-xl lg:text-2xl mb-8 text-black text-3xl">
@@ -135,13 +159,13 @@ export default function Home() {
                 key={index}
                 onClick={() => handleAnswer(index)}
                 className={`w-full p-2 rounded-lg transition-all text-black text-xl shadow-md ${
-                    selectedAnswer === index ? 
-                      reponse.correcte ? 
-                        'bg-green-500' 
-                        : 'bg-red-500' 
-                      : selectedAnswer !== null && reponse.correcte ? 
+                    selectedAnswer === index ?
+                      reponse.correcte ?
                         'bg-green-500'
-                        : 'bg-white hover:bg-gray-300'
+                        : 'bg-red-500'
+                      : selectedAnswer !== null && reponse.correcte ?
+                        'bg-green-500'
+                        : 'bg-white md:hover:bg-gray-300'
                 }`}
                 disabled={selectedAnswer !== null}
               >
@@ -152,7 +176,7 @@ export default function Home() {
 
         {/* Counter */}
         <div className="text-center text-md text-gray-600">
-          Bonnes réponses: {score.bonnes} <br/> 
+          Bonnes réponses: {score.bonnes} <br/>
           Mauvaises réponses: {score.mauvaises}
         </div>
 
@@ -173,7 +197,6 @@ export default function Home() {
   );
 }
 
-
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     // Choisir un index aléatoire entre 0 et i
@@ -183,4 +206,3 @@ function shuffleArray(array) {
   }
   return array;
 }
-
